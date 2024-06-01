c815e491a972c56504e5108e2d2f3de8dc4af6d3
AfazPaul1 <afazpaul@gmail.com>
2024-06-01 22:36:47 +0530

    used bulma to style and added descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,46 @@ import SiriImage from "./images/siri.png"
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
-            <ProfileCard title="Cortana" handle="@cortana22" image={CortanaImage}/>
-            <ProfileCard title="siri" handle="@siri01" image={SiriImage}/>
+            <section className="hero i-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                        <ProfileCard 
+                        title="Alexa" 
+                        handle="@alexa99" 
+                        image={AlexaImage}
+                        description="Amazon's voice-controlled assistant that powers smart home devices and provides information, entertainment, and assistance."
+
+                        />
+                        </div>
+                        <div className="column is-4">
+                        <ProfileCard 
+                        title="Cortana" 
+                        handle="@cortana22" 
+                        image={CortanaImage}
+                        description="Microsoft's virtual assistant designed to help with tasks, reminders, and information retrieval."
+                        />
+
+                        </div>
+                        <div className="column is-4">
+                        <ProfileCard 
+                        title="siri" 
+                        handle="@siri01" 
+                        image={SiriImage}
+                        description="    Apple's intelligent assistant that responds to voice commands for performing various tasks on Apple devices."
+                        />
+
+                        </div>
+
+                    </div>
+                </section>
+            </div>
+
         </div>
     )
 }
